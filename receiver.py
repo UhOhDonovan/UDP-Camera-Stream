5c2c932ac1c04bccf1e659127853a1198527f02c
Donovan Bale <donovan@balehaus.org>
2024-04-23 22:55:33 -0500

    Changed sender to server and vias versa

--- receiver.py
+++ receiver.py
@@ -4,42 +4,83 @@ import cv2
 import logging
 import sys as sus
 import datetime
-import time
 
 logging.basicConfig(
     filename=f"./tmp/{datetime.datetime.now()}-receiver.log",
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s %(levelname)s %(name)s %(lineno)d %(message)s",
 )
 logger = logging.getLogger(__name__)
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 9999
-CHUNK_SIZE = 46080 # 576  # size of each chunk
-NUM_CHUNKS = 20 # 1600
+SENDER_IP = "127.0.0.1"
+SENDER_PORT = 12000
 
+def parse_args():
+    if len(sus.argv) != 5:
+        RECEIVER_IP = "127.0.0.1"
+        RECEIVER_PORT = 9999
+        CHUNK_SIZE = 46080 # 576  # size of each chunk
+        NUM_CHUNKS = 20 # 1600
+    else:
+        RECEIVER_IP = sus.argv[1]
+        RECEIVER_PORT = int(sus.argv[2])
+        CHUNK_SIZE = int(sus.argv[3])  # 576  # size of each chunk
+        NUM_CHUNKS = int(sus.argv[4])  # 1600
+        assert(CHUNK_SIZE * NUM_CHUNKS == 921600)
+    return RECEIVER_IP, RECEIVER_PORT, CHUNK_SIZE, NUM_CHUNKS
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+def receive(RECEIVER_IP, RECEIVER_PORT, CHUNK_SIZE, NUM_CHUNKS):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((RECEIVER_IP, RECEIVER_PORT))
 
-dummy_chunk = np.zeros(CHUNK_SIZE, dtype=np.uint8)
-data_sequence = np.zeros(CHUNK_SIZE * NUM_CHUNKS, dtype=np.uint8)
-
-while True:
+    # dummy_chunk = np.zeros(CHUNK_SIZE, dtype=np.uint8)
+    data_sequence = np.zeros(CHUNK_SIZE * NUM_CHUNKS, dtype=np.uint8)
     try:
-        data, addr = sock.recvfrom(CHUNK_SIZE + 3)
-        sequence_number = int.from_bytes(data[0:3], "big")
-        frame_data = data[3:]
-        data_sequence[
-            sequence_number * CHUNK_SIZE : (sequence_number + 1) * CHUNK_SIZE
-        ] = (np.frombuffer(frame_data, dtype=np.uint8))
-        frame = data_sequence.reshape(480, 640, 3)
-        cv2.imshow("receiver", frame)
+        while True:
+            data, addr = sock.recvfrom(CHUNK_SIZE + 3)
+            sequence_number = int.from_bytes(data[0:3], "big")
+            frame_data = data[3:]
+            data_sequence[
+                sequence_number * CHUNK_SIZE : (sequence_number + 1) * CHUNK_SIZE
+            ] = np.frombuffer(frame_data, dtype=np.uint8)
+            frame = data_sequence.reshape(480, 640, 3)
+            cv2.imshow("receiver", frame)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
     except Exception as e:
         ex_type, ex, tb = sus.exc_info()
         logger.error(f"{e} {tb.tb_lineno}")
-        sus.exit()
+    finally:
+        sock.close()
+    
+
+def initiate_connection(RECEIVER_IP, RECEIVER_PORT, CHUNK_SIZE, NUM_CHUNKS):
+    try:
+        client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        client_socket.connect((SENDER_IP, SENDER_PORT))
+        request = f"{RECEIVER_IP},{RECEIVER_PORT}"
+        client_socket.send(request.encode())
+
+        data = b""
+        while True:
+            chunk = client_socket.recv(1024)
+            if not chunk:
+                break
+
+            data += chunk
+
+        assert data.decode() == "OK"
+        receive(RECEIVER_IP, RECEIVER_PORT, CHUNK_SIZE, NUM_CHUNKS)
+    finally:
+        client_socket.close()
+
+
+def main():
+    RECEIVER_IP, RECEIVER_PORT, CHUNK_SIZE, NUM_CHUNKS = parse_args()
+    initiate_connection(RECEIVER_IP, RECEIVER_PORT, CHUNK_SIZE, NUM_CHUNKS)
+
+
+if __name__ == "__main__":
+    main()
--- sender.py
+++ sender.py
@@ -3,30 +3,74 @@ import numpy as np
 import cv2
 import time
 import random
+import threading
 
 # Specify IP information
-UDP_IP = "127.0.0.1"
-UDP_PORT = 9999
+LISTENER_IP = "127.0.0.1"
+LISTENER_PORT = 12000
 CHUNK_SIZE = 46080  # 576
 NUM_CHUNKS = 20  # 1600
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-cap = cv2.VideoCapture(0)  # Capture video element
-try:
-    while True:
-        _, frame = cap.read()
-        d = frame.flatten()
-        numbers = list(range(NUM_CHUNKS))
-        random.shuffle(numbers)
-        for i in numbers:
-            sequence_number = i.to_bytes(3, "big")
-            start_idx = i * CHUNK_SIZE
-            end_idx = (i + 1) * CHUNK_SIZE
-            chunk = d[start_idx:end_idx]
-            sock.sendto(sequence_number + chunk.tobytes(), (UDP_IP, UDP_PORT))
+def listen(connection: socket.socket, addr: tuple[str, int]):
+    try:
+        msg = connection.recv(1024)
+        RECEIVER_IP, RECIEVER_PORT = msg.decode().split(",")
+        response = f"OK"
+        connection.send(response.encode())
+        connection.close()
+        send_to_receiver(RECEIVER_IP, int(RECIEVER_PORT))
+    except Exception as e:
+        print(e)
+    finally:
+        connection.close()
 
-        time.sleep(0.05)        
 
-except KeyboardInterrupt:
-    cap.release()
-    cv2.destroyAllWindows()
+
+
+def send_to_receiver(RECEIVER_IP: str, RECEIVER_PORT: int):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cap = cv2.VideoCapture(0)  # Capture video element
+    try:
+        while True:
+            _, frame = cap.read()
+            d = frame.flatten()
+            numbers = list(range(NUM_CHUNKS))
+            random.shuffle(numbers)
+            for i in numbers:
+                sequence_number = i.to_bytes(3, "big")
+                start_idx = i * CHUNK_SIZE
+                end_idx = (i + 1) * CHUNK_SIZE
+                chunk = d[start_idx:end_idx]
+                sock.sendto(sequence_number + chunk.tobytes(), (RECEIVER_IP, RECEIVER_PORT))
+
+            time.sleep(0.05)
+
+    except KeyboardInterrupt:
+        cap.release()
+        cv2.destroyAllWindows()
+    
+    finally:
+        sock.close()
+
+def main():
+    listener_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    listener_socket.bind((LISTENER_IP, LISTENER_PORT))
+    listener_socket.listen(2)
+
+    threads = []
+    try: 
+        while True:
+            connection, addr = listener_socket.accept()
+            
+            new_thread = threading.Thread(target=listen, args=(connection, addr))
+            new_thread.start()
+
+            threads.append(new_thread)
+    except Exception as e:
+        print(e)
+    finally:
+        listener_socket.close()
+
+if __name__ == "__main__":
+    main()
